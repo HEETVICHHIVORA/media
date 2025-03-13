48399021a40044ad7a5bbbaf87df23411e591213
Paras Waghela <paraswaghela777@gmail.com>
2025-03-13 22:44:30 +0530

    Service2 to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import './App.css'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import { Homepage } from './pages/Homepage'
 import { Services } from './pages/Services'
-import { Services2 } from './pages/Services2'
-import { Services3 } from './pages/Services3'
 import {Contactus} from './pages/Contactus';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BlogPage from './pages/BlogPage';
 import AboutMediatryx from './pages/Aboutmediatryx'
 function App() {
@@ -15,8 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage/>} />
         <Route path="/services" element={<Services/>} />
-        <Route path="/services2" element={<Services2/>} />
-        <Route path="/services3" element={<Services3/>} />
         <Route path="/blog/:id" element={<BlogPage />} />
         <Route path="services/contactus/:service" element={<Contactus />} />
         <Route path="/aboutmediatryx"  element={<AboutMediatryx/>} />
